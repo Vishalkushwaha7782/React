5eddc1e505823661ac95c380f4767eb8506d9dc0
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-23 14:34:55 +0530

    reactrouter(change images)

---
+++ reactRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { Layout as RouterLayout } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
